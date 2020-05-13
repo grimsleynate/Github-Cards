9dc5bd5877748fe74cd223b1b0c7555774d66660
grimsleynate <grimsleynate@gmail.com>
2020-05-13 13:50:02 -0400

    Working on Form button

--- src/index.js
+++ src/index.js
@@ -5,22 +5,11 @@ import * as serviceWorker from './serviceWorker';
 
 //Test data in an array, used to make sure our cards fill out properly
 const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "Facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Facebook"},
-      {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-      {name: "Nathaniel Grimsley", avatar_url: "https://avatars3.githubusercontent.com/u/30242518?v=4", company: "Deadhead Media"}
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "Facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Facebook"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+  {name: "Nathaniel Grimsley", avatar_url: "https://avatars3.githubusercontent.com/u/30242518?v=4", company: "Deadhead Media"}
 ];
-
-//CardList component that returns an array of Cards
-const CardList = (props) => {
-  return (<div> 
-    {/* .map() creates a new array out of an established array.
-      This creates an array of Card components, and pushes an
-      object from testData into each*/}
-    {testData.map(profile=><Card {...profile}/>)}
-  </div>);
-};
-
 //Card component for holding all the information about each person.
 class Card extends React.Component {
   render () {
@@ -37,13 +26,36 @@ class Card extends React.Component {
   }
 }
 
+//CardList component that returns an array of Cards
+const CardList = (props) => {
+  return (<div> 
+    {/* .map() creates a new array out of an established array.
+      This creates an array of Card components, and pushes an
+      object from testData into each*/}
+    {props.profiles.map(profile=><Card {...profile}/>)}
+  </div>);
+};
+
 //Form manages user input
 class Form extends React.Component {
+  //every function comes with an event argument, you can name it whatever you want.  
+  //this allows you to use native event-related functions
+  handleSubmit = (event) => {
+    //this prevents the page from refreshing when you submit.
+    event.preventDefault();
+    console.log();
+  };
+
   render() {
     return (
       <div className="form">
-        <form action="">
-          <input type="text" placeholder="Github username"/>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="Github username" 
+            ref={this.userNameInput}
+            required 
+          />
           <button>Add Card</button>
         </form>
       </div>
@@ -53,12 +65,16 @@ class Form extends React.Component {
 
 //App is going to be the container for the whole app.
 class App extends React.Component {
+  state = {
+    profiles: testData,
+  }
+
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
         <Form />
-        <CardList />
+        <CardList profiles={this.state.profiles} />
       </div>
     );
   }
